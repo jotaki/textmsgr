9644b21bffba36d2086a923e3af93a15e65968fd
Joseph Kinsella <jkinsella@spiryx.net>
2019-03-18 09:27:39 -0400

    another commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   def login
   end
 
-  def real_login
+  def post_login
     @user = User.get(params.fetch('name'), params.fetch('phone_number'))
-    render 'real_login', locals: { user: @user }
+    render 'post_login', locals: { user: @user }
   end
 end
